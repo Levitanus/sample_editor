f0fdf1b44dba47c9e4872411c66e3c21c164d78a
Timofey <pianoist@ya.ru>
2021-01-10 05:13:31 +0700

    setup (not finished)

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='sample_editor',
-    version='1.0',
-    description='A useful module',
-    author='Man Foo',
-    author_email='foomail@foo.com',
-    packages=['sample_editor', 'sample_editor/sub_package'],  # same as name
-    package_data={'sample_editor': ['py.typed']}
-    # install_requires=['bar', 'greek'], #external packages as dependencies
+    version='0.1',
+    description='Number of tools for sample cutting, render and anylyzing'
+    'in flexible GUI wrapping. Requires REAPER.',
+    author='Levitanus',
+    author_email='pianoist@ya.ru',
+    packages=['sample_editor'],  # same as name
+    package_data={'sample_editor': ['py.typed']},
+    install_requires=['aenum', 'librosa', 'python-reapy'],
 )
