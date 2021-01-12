70db268586d633e8625b6d08ea312f26aee8cedb
Timofey <pianoist@ya.ru>
2021-01-13 04:46:01 +0700

    draft setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,9 @@ setup(
     'in flexible GUI wrapping. Requires REAPER.',
     author='Levitanus',
     author_email='pianoist@ya.ru',
+    entry_points={
+        'console_scripts': ['sample_editor = sample_editor.__main__:main']
+    },
     packages=['sample_editor'],  # same as name
     package_data={'sample_editor': ['py.typed']},
     install_requires=['aenum', 'librosa', 'python-reapy'],
