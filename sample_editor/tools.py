f655a40679d7701b1ac9e43d2c328d7c4c4dd3ee
Timofey <pianoist@ya.ru>
2021-01-21 22:53:04 +0700

    Reorganized tools: hz_to_note, length_convert, LengthUnit
    hz_to_note needed for fixing librosa output with symbol '♯' which is not counted as '#' by samplers

--- sample_editor/tools.py
+++ sample_editor/tools.py
@@ -1,53 +1,89 @@
+from enum import Enum, auto
 import math
 import typing as ty
 import reapy as rpr
 from types import TracebackType
+import librosa as lr
 
 
-class InsideUndoContext:
-    """Combines reapy context managers for insidee reaper and undo.
+class LengthUnit(Enum):
+    samples = auto()
+    ms = auto()
+    frames = auto()
+
+
+def length_convert(
+    length: float,
+    sr: int,
+    units_def: LengthUnit,
+    units_target: LengthUnit,
+    hop_length: int = 512
+) -> float:
+    """Convert length from one unit to another.
 
     Parameters
     ----------
-    undo_name : str
-        Undo block description.
-    flags : int
-        1: track configurations
-        2: track FX
-        4: track items
-        8: project states
-        16: freeze states
+    length : float
+        in sec
+    sr : int
+    units_def : LengthUnit
+        Units that are passed
+    units_target : LengthUnit
+        Units that are expected
+    hop_length : int, optional
+        512 by default, mandatory for frames conversion
+
+    Returns
+    -------
+    float
     """
+    if units_def == LengthUnit.samples:
+        if units_target == LengthUnit.frames:
+            return lr.samples_to_frames(length, hop_length)  # type:ignore
+        if units_target == LengthUnit.ms:
+            return lr.samples_to_time(length, sr)  # type:ignore
+        return length
+    if units_def == LengthUnit.ms:
+        if units_target == LengthUnit.samples:
+            return lr.time_to_samples(length, sr)  # type:ignore
+        if units_target == LengthUnit.frames:
+            return lr.time_to_frames(length, sr, hop_length)  # type:ignore
+        return length
+    if units_def == LengthUnit.frames:
+        if units_target == LengthUnit.samples:
+            return lr.frames_to_samples(length, hop_length)  # type:ignore
+        if units_target == LengthUnit.ms:
+            return lr.frames_to_time(length, sr, hop_length)  # type:ignore
+        return length
+    raise TypeError(f'not a LengthUnit: {units_def, units_target}')
 
-    def __init__(self, undo_name: str, flags: int = 0) -> None:
-        """
-        Parameters
-        ----------
-        undo_name : str
-            Undo block description.
-        flags : int
-            1: track configurations
-            2: track FX
-            4: track items
-            8: project states
-            16: freeze states
-        """
-        self._u_text = undo_name
-        self._u_flags = flags
 
-    def __enter__(self) -> 'InsideUndoContext':
-        self._ir = rpr.inside_reaper()
-        self._ub = rpr.undo_block(self._u_text, flags=self._u_flags)
-        self._ir.__enter__()
-        self._ub.__enter__()
-        print('__enter__')
-        return self
+@ty.overload
+def hz_to_note(freq: float) -> str:
+    ...
 
-    def __exit__(
-        self, exc_type: ty.Optional[ty.Type[BaseException]],
-        value: ty.Optional[BaseException],
-        traceback: ty.Optional[TracebackType]
-    ) -> None:
-        self._ir.__exit__(exc_type, value, traceback)
-        self._ub.__exit__(exc_type, value, traceback)
-        print('__exit__')
+
+@ty.overload
+def hz_to_note(freq: ty.Iterable[float]) -> ty.List[str]:
+    ...
+
+
+def hz_to_note(
+    freq: ty.Union[float, ty.Iterable[float]]
+) -> ty.Union[str, ty.List[str]]:
+    """Fork of librosa.hz_to_note with correct '#' symbol.
+
+    Parameters
+    ----------
+    freq : Union[float, Iterable[float]]
+        freq in hz
+
+    Returns
+    -------
+    Union[str, List[str]]
+        Note name in format "C#2". Middle C is "C4".
+    """
+    result = ty.cast(ty.Union[str, ty.List[str]], lr.hz_to_note(freq))
+    if isinstance(result, ty.List):
+        return [note.replace('♯', '#') for note in result]
+    return result.replace('♯', '#')
